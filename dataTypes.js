7b663de8f956ab91ff1a2cb1b734409207ac5824
SupriyoGhatak <supriyo.ghatak.cse21@heritageit.edu.in>
2024-03-10 12:57:35 +0530

    datTypes

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+// primitive data types
+
+/*
+    Number
+    String
+    Boolean
+    null
+    undefined
+    BigInt
+    Symbol
+*/
+
+//Non Primitive dataTypes
+
+/*
+    Object
+*/
+
+const students = {
+    firstName : "Supriyo",
+    age: 26,
+    collegePassout : true
+};
+
+console.log(students.collegePassout);
