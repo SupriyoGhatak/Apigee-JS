9f747859f0ac6771e51cc18fd4c144108c402c6a
SupriyoGhatak <supriyo.ghatak.cse21@heritageit.edu.in>
2024-03-10 13:02:25 +0530

    miniObjectProject

---
+++ project1.js
@@ -0,0 +1,20 @@
+const product = {
+    productName: "Pen",
+    productPrice : 25,
+    productOffer : "25%",
+    productRatting: 4.1
+};
+
+console.log(product);
+
+
+const instagram = {
+
+    profileName: "Supriyo",
+    profileFollowers: 1205,
+    profileNoOfPost: 353,
+    profileJobTitle: "Software Engineer",
+    profileFollowings: 100
+};
+
+console.log(instagram);
